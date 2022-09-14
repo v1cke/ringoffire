161e7f68a0cd11a1d12d61a17d020368eccf968d
v1cke <v1cke1982@gmail.com>
2022-09-14 17:30:06 +0200

    add player

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Hans', 'Pete', 'Kosta', 'Vicke'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
